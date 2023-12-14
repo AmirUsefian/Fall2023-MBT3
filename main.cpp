07292491323e18c8106889b7cefc3bd66801e7c6
Amir <140272797+AmirUsefian@users.noreply.github.com>
2023-12-14 14:12:41 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,18 +10,15 @@ public:
     MatrixOperations() {}
 
     void run() {
-        // گرفتن اندازه ماتریس از کاربر
         int rows, cols;
         cout << "Enter number of rows: ";
         cin >> rows;
         cout << "Enter number of columns: ";
         cin >> cols;
 
-        // تعریف ماتریس با اندازه داده شده
         MatrixXd mat1(rows, cols);
         MatrixXd mat2(rows, cols);
 
-        // گرفتن مقادیر ماتریس از کاربر
         cout << "Enter values for matrix 1:" << endl;
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
@@ -36,17 +33,14 @@ public:
             }
         }
 
-        // عملیات جمع و تفریق و ضرب بر روی ماتریس‌ها
         MatrixXd sum = mat1 + mat2;
         MatrixXd sub = mat1 - mat2;
         MatrixXd mul = mat1 * mat2;
 
-        // نمایش نتایج
         cout << "Sum:" << endl << sum << endl;
         cout << "Subtraction:" << endl << sub << endl;
         cout << "Multiplication:" << endl << mul << endl;
 
-        // تعیین اندازه ماتریس برای determinant
         if (rows == 2 && cols == 2) {
             double det1 = mat1(0, 0) * mat1(1, 1) - mat1(0, 1) * mat1(1, 0);
             double det2 = mat2(0, 0) * mat2(1, 1) - mat2(0, 1) * mat2(1, 0);
@@ -59,7 +53,6 @@ public:
             cout << "Cannot calculate determinant" << endl;
         }
 
-        // عملیات inverse با استفاده از کتابخانه eigen
         if (rows == cols) {
             MatrixXd inv1 = mat1.inverse();
             MatrixXd inv2 = mat2.inverse();
