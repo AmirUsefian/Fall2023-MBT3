d097a1086250ef0a1f0d171d61adee5b1142ae73
amir <amiryn996@gmail.com>
2023-12-14 14:09:36 +0330

    main file added

--- main.cpp
+++ main.cpp
@@ -1,182 +1,77 @@
 #include <iostream>
+#include <Eigen/Dense>
+
 using namespace std;
 
-class Matrix {
-private:
-    int rows, cols;
-    int** data;
+using namespace Eigen;
 
+class MatrixOperations {
 public:
-    Matrix(int r, int c) {
-        rows = r;
-        cols = c;
-        data = new int*[rows];
-        for (int i = 0; i < rows; i++) {
-            data[i] = new int[cols];
-        }
-    }
+    MatrixOperations() {}
 
-    void inputMatrix() {
-        cout << "Enter the elements of the matrix:" << endl;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                cin >> data[i][j];
-            }
-        }
-    }
+    void run() {
+        // گرفتن اندازه ماتریس از کاربر
+        int rows, cols;
+        cout << "Enter number of rows: ";
+        cin >> rows;
+        cout << "Enter number of columns: ";
+        cin >> cols;
 
-    void displayMatrix() {
+        // تعریف ماتریس با اندازه داده شده
+        MatrixXd mat1(rows, cols);
+        MatrixXd mat2(rows, cols);
+
+        // گرفتن مقادیر ماتریس از کاربر
+        cout << "Enter values for matrix 1:" << endl;
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                cout << data[i][j] << " ";
+                cin >> mat1(i, j);
             }
-            cout << endl;
         }
-    }
 
-    Matrix add(Matrix m) {
-        Matrix result(rows, cols);
+        cout << "Enter values for matrix 2:" << endl;
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                result.data[i][j] = data[i][j] + m.data[i][j];
+                cin >> mat2(i, j);
             }
         }
-        return result;
-    }
 
-    Matrix subtract(Matrix m) {
-        Matrix result(rows, cols);
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < cols; j++) {
-                result.data[i][j] = data[i][j] - m.data[i][j];
-            }
-        }
-        return result;
-    }
+        // عملیات جمع و تفریق و ضرب بر روی ماتریس‌ها
+        MatrixXd sum = mat1 + mat2;
+        MatrixXd sub = mat1 - mat2;
+        MatrixXd mul = mat1 * mat2;
 
-    
+        // نمایش نتایج
+        cout << "Sum:" << endl << sum << endl;
+        cout << "Subtraction:" << endl << sub << endl;
+        cout << "Multiplication:" << endl << mul << endl;
 
-     Matrix multiply(Matrix m) {
-        Matrix result(rows, m.cols);
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < m.cols; j++) {
-                result.data[i][j] = 0;
-                for (int k = 0; k < cols; k++) {
-                    result.data[i][j] += data[i][k] * m.data[k][j];
-                }
-            }
-        }
-        return result;
-    }
-
-     Matrix determine(Matrix) {
+        // تعیین اندازه ماتریس برای determinant
         if (rows == 2 && cols == 2) {
-            Matrix result(1, 1);
-            result.data[0][0] = data[0][0] * data[1][1] - data[0][1] * data[1][0];
-            return result;
+            double det1 = mat1(0, 0) * mat1(1, 1) - mat1(0, 1) * mat1(1, 0);
+            double det2 = mat2(0, 0) * mat2(1, 1) - mat2(0, 1) * mat2(1, 0);
+            cout << "Determinant 1: " << det1 << endl << "Determinant 2: " << det2 << endl;
         } else if (rows == 3 && cols == 3) {
-            Matrix result(1, 1);
-            result.data[0][0] = data[0][0] * data[1][1] * data[2][2] +
-                                data[0][1] * data[1][2] * data[2][0] +
-                                data[0][2] * data[1][0] * data[2][1] -
-                                data[0][2] * data[1][1] * data[2][0] -
-                                data[0][1] * data[1][0] * data[2][2] -
-                                data[0][0] * data[1][2] * data[2][1];
-            return result;
+            double det1 = mat1.determinant();
+            double det2 = mat2.determinant();
+            cout << "Determinant 1: " << det1 << endl << "Determinant 2: " << det2 << endl;
         } else {
-            cout << "Cannot determine for this matrix size" << endl;
-            exit(1);
+            cout << "Cannot calculate determinant" << endl;
+        }
+
+        // عملیات inverse با استفاده از کتابخانه eigen
+        if (rows == cols) {
+            MatrixXd inv1 = mat1.inverse();
+            MatrixXd inv2 = mat2.inverse();
+            cout << "Inverse 1 : " << endl << inv1 << endl << "Inverse 2 : " << inv2 << endl; 
+        } else {
+            cout << "Cannot calculate inverse" << endl;
         }
     }
-
-    Matrix inverse(Matrix) {
-        if (rows == 2 && cols == 2) {
-            Matrix result(rows, cols);
-            int det = data[0][0] * data[1][1] - data[0][1] * data[1][0];
-            if (det == 0) {
-                cout << "Inverse does not exist" << endl;
-                exit(1);
-            } else {
-                result.data[0][0] = data[1][1] / det;
-                result.data[0][1] = -data[0][1] / det;
-                result.data[1][0] = -data[1][0] / det;
-                result.data[1][1] = data[0][0] / det;
-                return result;
-            }
-        } else if (rows == 3 && cols == 3) {
-            Matrix result(rows, cols);
-            int det =
-                data[0][0] * (data[1][1] * data[2][2] - data[1][2] * data[2][1]) -
-                data[0][1] * (data[1][0] * data[2][2] - data[1][2] * data[2][0]) +
-                data[0][2] * (data[1][0] * data[2][1] - data[1][1] * data[2][0]);
-            if (det == 0) {
-                cout << "Inverse does not exist" << endl;
-                exit(1);
-            } else
-
-{
-                result.data[0][0] = (data[1][1]*data[2][2]-data[1][2]*data[2][1]) / det;
-                result.data[0][1] = (data[0][2]*data[2][1]-data[0][1]*data[2][2]) / det;
-                result.data[0][2] = (data[0][1]*data[1][2]-data[0][2]*data[1][1]) / det;
-                result.data[1][0] = (data[1][2]*data[2][0]-data[1][0]*data[2][2]) / det;
-                result.data[1][1] = (data[0][0]*data[2][2]-data[0][2]*data[2][0]) / det;
-                result.data[1][2] = (data[0][2]*data[1][0]-data[0][0]*data[1][2]) / det;
-                result.data[2][0] = (data[1][0]*data[2][1]-data[1][1]*data[2][0]) / det;
-                result.data[2][1] = (data[0][1]*data[2][0]-data[0][0]*data[2][1]) / det;
-                result.data[2][2] = (data[0][0]*data[1][1]-data[0][1]*data[1][0]) / det;
-                return result;
-            }
-        } else {
-            cout << "Cannot find inverse for this matrix size" << endl;
-            exit(1);
-        }
-    }
-
-
-    
 };
 
 int main() {
-    int choice, r, c;
-    cout << "Enter the number of rows and columns for the matrix: ";
-    cin >> r >> c;
-
-    Matrix matrix(r, c);
-    matrix.inputMatrix();
-
-    cout << "Choose an operation to perform on the matrix:" << endl;
-    cout << "1. Add" << endl;
-    cout << "2. Subtract" << endl;
-    cout << "3. Multiply" << endl;
-    cout << "4. Determine" << endl;
-    cout << "5. Inverse" << endl;
-    cin >> choice;
-
-    Matrix result(r, c);
-    switch (choice) {
-        case 1:
-            result = matrix.add(matrix);
-            break;
-        case 2:
-            result = matrix.subtract(matrix);
-            break;
-        case 3:
-            result = matrix.multiply(matrix);
-            break;
-        case 4:
-            result = matrix.determine(matrix);
-            break;
-        case 5:
-             result = matrix.inverse(matrix);   
-             break;
-
-        default:
-            cout << "Invalid choice!" << endl;
-            break;
-    }
-
-    cout << "Result:" << endl;
-    result.displayMatrix();
-
+    MatrixOperations matOps;
+    matOps.run();
     return 0;
 }
